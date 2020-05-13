a495012bf1d42f6945015299f9013f89af170772
lyp <zyx_2017@163.com>
2020-05-13 08:01:13 +0800

    增加配置文件加密，加密数据密码，但是加密邮件密码失败，不能发送邮件，后续再改进

--- src/main/java/com/lyp/springboot01/test/controller/JasyptController.java
+++ src/main/java/com/lyp/springboot01/test/controller/JasyptController.java
@@ -1,4 +1,21 @@
 package com.lyp.springboot01.test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 加密测试
+ */
+@RestController
 public class JasyptController {
+
+    @Value("${mail.auth.password}")
+    private String password;
+
+    @RequestMapping("/hello")
+    public String testJasypt() {
+        return password;
+    }
 }
